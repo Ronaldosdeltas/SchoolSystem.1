dd42a99a33231d02ddaad608497e83e2a07711fc
ronaldoss8 <ronaldosssantos1@gmail.coml>
2024-12-21 23:55:04 -0300

    settin up the atributs

--- src/main/java/backfirststeps/model/Person.java
+++ src/main/java/backfirststeps/model/Person.java
@@ -1,4 +1,11 @@
 package backfirststeps.model;
 
-public class Person {
+public abstract class Person {
+    private String name;
+    private int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 }
--- src/main/java/backfirststeps/model/Student.java
+++ src/main/java/backfirststeps/model/Student.java
@@ -1,4 +1,14 @@
 package backfirststeps.model;
 
-public class Student {
+import java.util.ArrayList;
+
+public class  Student extends Person {
+    private ArrayList<Metter> metter;
+    private int DataMatricula;
+
+    public Student(String name, int id, ArrayList<Metter> metter, int dataMatricula) {
+        super(name, id);
+        this.metter = metter;
+        DataMatricula = dataMatricula;
+    }
 }
